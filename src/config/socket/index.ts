58146ff0058f9192d30f4c5a9c8c5db4b79d58a9
Eric Jobe <ericjobe@Erics-MacBook-Pro.local>
2023-10-12 17:16:41 -0500

    temp harcode server

--- src/config/socket/index.ts
+++ src/config/socket/index.ts
@@ -1,7 +1,8 @@
 import { createContext } from 'react';
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_SERVER_URL;
+// const URL = import.meta.env.VITE_SERVER_URL;
+const URL = 'https://baton-server.onrender.com:4000';
 
 export const socket = io(URL).connect();
 export const SocketContext = createContext(socket);
